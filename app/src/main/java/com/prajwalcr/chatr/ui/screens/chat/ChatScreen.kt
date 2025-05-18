404362111695f28f6847694d41a44b30e4e93069
prajwalcr <prajwalcr006@gmail.com>
2025-05-18 15:51:16 +0530

    Adjusted UI push on launch of onScreen keyboard

--- app/src/main/java/com/prajwalcr/chatr/ui/screens/chat/ChatScreen.kt
+++ app/src/main/java/com/prajwalcr/chatr/ui/screens/chat/ChatScreen.kt
@@ -2,13 +2,20 @@ package com.prajwalcr.chatr.ui.screens.chat
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
@@ -23,6 +30,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.prajwalcr.domain.model.Message
 import com.prajwalcr.domain.model.UserData
@@ -34,9 +43,13 @@ import timber.log.Timber
 @Composable
 fun ChatScreen(channelId: String) {
 
+    val currentKeyBoard = LocalSoftwareKeyboardController.current
+
     val messageText = remember {
         mutableStateOf("")
     }
+
+    val scrollState = rememberScrollState()
 
     var currentUserData = remember {
         mutableStateOf<UserData?>(null)
@@ -51,9 +64,17 @@ fun ChatScreen(channelId: String) {
         chatViewModel.listenForMessages(channelId)
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            Timber.d("messageList.value = ${messageList.value}")
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(scrollState)
+        .imePadding()
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(20.dp)
+                .weight(1f),
+            reverseLayout = true
+        ) {
+            Timber.d("messageList.value = ${messageList.value}")  //keep this line
             Timber.tag("cptn_test").d("currentUser = $currentUserData")
             items(messageList.value) { message ->
                 Timber.tag("cptn_test_inside").d("currentUser = $currentUserData")
@@ -69,21 +90,27 @@ fun ChatScreen(channelId: String) {
                 .fillMaxWidth()
                 .background(color = Color.Gray)
                 .padding(8.dp)
-                .align(Alignment.BottomCenter)
         ) {
             TextField(
                 value = messageText.value,
                 onValueChange = { messageText.value = it },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        currentKeyBoard?.hide()
+                    }
+                )
             )
-
             IconButton(
                 onClick = {
-                    chatViewModel.sendMessage(
-                        channelId,
-                        messageText.value
-                    )
-                    messageText.value = ""
+                    if (messageText.value.isNotEmpty()) {
+                        chatViewModel.sendMessage(
+                            channelId,
+                            messageText.value
+                        )
+                        messageText.value = ""
+                    }
                 },
             ) {
                 Icon(
