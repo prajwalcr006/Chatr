33cc5d29c238305ee8499f5294188ad29fb49fc5
prajwalcr <prajwalcr006@gmail.com>
2025-05-24 18:58:39 +0530

    testing cherry pick

--- app/src/main/java/com/prajwalcr/chatr/ui/screens/chat/ChatScreen.kt
+++ app/src/main/java/com/prajwalcr/chatr/ui/screens/chat/ChatScreen.kt
@@ -1,18 +1,22 @@
 package com.prajwalcr.chatr.ui.screens.chat
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -30,10 +34,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,9 +48,12 @@ import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.prajwalcr.chatr.ui.BlueBlack
+import com.prajwalcr.chatr.ui.rememberImeState
 import com.prajwalcr.domain.model.Message
 import com.prajwalcr.domain.model.UserData
 import com.prajwalcr.domain.repository.FirebaseAuthRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.distinctUntilChanged
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 import timber.log.Timber
@@ -57,7 +67,9 @@ fun ChatScreen(channelId: String) {
         mutableStateOf("")
     }
 
-    val scrollState = rememberScrollState()
+    val imeState = rememberImeState()
+
+    val listState = rememberLazyListState()
 
     var currentUserData = remember {
         mutableStateOf<UserData?>(null)
@@ -72,23 +84,38 @@ fun ChatScreen(channelId: String) {
         chatViewModel.listenForMessages(channelId)
     }
 
+    LaunchedEffect(messageList.value) {
+        if ((messageList.value.isNotEmpty()) && (messageList.value.last().senderId == currentUserData.value?.userId)) {
+            listState.animateScrollToItem(messageList.value.lastIndex)
+        }
+    }
+
+    LaunchedEffect(imeState.value) {
+        if (imeState.value) {
+            snapshotFlow { listState.layoutInfo.viewportEndOffset }
+                .distinctUntilChanged()
+                .collect {
+                    val lastVisibleItem = listState.layoutInfo.visibleItemsInfo.lastOrNull()
+                    if (lastVisibleItem?.index != messageList.value.lastIndex) {
+                        listState.animateScrollToItem(messageList.value.lastIndex)
+                    }
+                }
+        }
+    }
+
     Column(modifier = Modifier
         .fillMaxSize()
-        .verticalScroll(scrollState)
         .imePadding()
         .background(BlueBlack)
     ) {
         LazyColumn(
             modifier = Modifier.padding(20.dp)
                 .weight(1f),
-            reverseLayout = true
+            state = listState
         ) {
             Timber.d("messageList.value = ${messageList.value}")  //keep this line
-            Timber.tag("cptn_test").d("currentUser = $currentUserData")
             items(messageList.value) { message ->
-                Timber.tag("cptn_test_inside").d("currentUser = $currentUserData")
                 currentUserData.value?.let { userData ->
-                    Timber.tag("cptn_test").d("message = $message && current user = $userData")
                     ChatBubble(message = message, currentUser = userData)
                 }
             }
@@ -140,7 +167,7 @@ fun PreviewChatBubble() {
             text = "I am loki of asgard and I am assigned with glorious purpose and test ",
             senderId = "124",
             senderName = "Prajwal",
-            profileUrl = "abc"
+            profileUrl = "https://lh3.googleusercontent.com/a/ACg8ocKgVHpTYdQ859-zWqmnK5IO6eHa-LO4wJHZ2wadVo9Bgdxu3w"
         ),
         UserData(
             email = "email",
@@ -161,29 +188,45 @@ fun ChatBubble(message: Message, currentUser: UserData) {
     } else {
         Color.Red
     }
+    val arrangement = if (isCurrentUser) {Arrangement.End} else {Arrangement.Start}
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 4.dp)
+            .padding(vertical = 8.dp, horizontal = 4.dp),
+        horizontalArrangement = arrangement,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        val alignment = if (isCurrentUser) {Alignment.CenterEnd} else {Alignment.CenterStart}
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = alignment
-        ) {
-            Box(
-                modifier = Modifier.background(
-                    color = bubbleColor,
-                    shape = RoundedCornerShape(8.dp)
-                )
-            ) {
-                Text(
-                    text = message.text,
-                    color = Color.White,
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
+        if (!isCurrentUser) {
+            Spacer(Modifier.padding(3.dp))
+            GlideImage(
+                model = message.profileUrl,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+                    .padding(6.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .align(Alignment.CenterVertically),
+                contentScale = ContentScale.Crop
+            )
         }
+
+        ChatBubbleText(bubbleColor, message)
+    }
+}
+
+@Composable
+fun ChatBubbleText(bubbleColor: Color, message: Message) {
+    Box(
+        modifier = Modifier.background(
+            color = bubbleColor,
+            shape = RoundedCornerShape(8.dp)
+        )
+            .widthIn(max = LocalConfiguration.current.screenWidthDp.dp * 0.7f)
+    ) {
+        Text(
+            text = message.text,
+            color = Color.White,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
